1362b312741402b35ef5cd179a8703a1ff4b1112
oskrpko <52108061+oskrpko@users.noreply.github.com>
2019-06-28 09:06:30 -0400

    Update script

---
+++ update-git-dep-and-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pushd ../bz-commons-values
+MYVAR=$(grep MYVAR values.bzl | cut -f 2 -d\")
+MYVAR=$((MYVAR+1))
+echo "MYVAR=$MYVAR"
+sed -i -r "s/\".+\"/\"$MYVAR\"/g" values.bzl
+git add values.bzl
+git commit -m "Changed MYVAR to $MYVAR"
+git push
+popd
+
+bazel build //..
